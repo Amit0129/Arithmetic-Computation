acfa237f9a920fcc10f770e681bdba8e04c6d498
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-23 12:43:36 +0530

    Added:UC3_Second Computation

--- arthcomp.sh
+++ arthcomp.sh
@@ -3,3 +3,4 @@ read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
 echo "First Compute : a + b * c = $(( a + b * c ))"
+echo "Second Compute : a * b + c = $(( a * b + c ))"
