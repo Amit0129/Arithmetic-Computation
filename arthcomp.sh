900e16d3dacecb1e5e516270c4328881bad1e646
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-26 17:30:06 +0530

    Added: UC5_Forth Computation

--- arthcomp.sh
+++ arthcomp.sh
@@ -2,6 +2,4 @@ read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
-echo "First Compute : a + b * c = $(( a + b * c ))"
-echo "Second Compute : a * b + c = $(( a * b + c ))"
-echo "Third Compute :$c + $a / $b = $(( c + a / b ))"
+echo "$a % $b + $c = $(( a % b + c ))"
