fec286aee13c3e75ad5b617c0feccf18cb04bbc1
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-26 17:39:38 +0530

    Added: UC6_Store Result in Dictionary

--- arthcomp.sh
+++ arthcomp.sh
@@ -2,4 +2,20 @@ read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
-echo "$a % $b + $c = $(( a % b + c ))"
+
+first_computation=$(( a + b * c ))
+second_computation=$(( a * b + c ))
+third_computation=$(( c + a / b ))
+fourth_computation=$(( a % b + c ))
+echo "$a + $b * $c = $first_computation"
+echo "$a * $b + $c = $second_computation"
+echo "$c + $a / $b = $third_computation"
+echo "$a % $b + $c = $fourth_computation"
+
+declare -A result
+result[first_computation]=$first_computation
+result[second_computation]=$second_computation
+result[third_computation]=$third_computation
+result[fourth_computation]=$fourth_computation
+
+echo ${result[@]}
